42aa6718e641524832a60c63051b75b1d699baed
geekan <geekan@foxmail.com>
2016-06-15 23:01:29 +0800

    force zh-cn

--- general_spider/general_spider/spiders/wiki_list_of_american_tv.py
+++ general_spider/general_spider/spiders/wiki_list_of_american_tv.py
@@ -10,7 +10,7 @@ class Config:
     allowed_domains=['wikipedia.org']
     start_urls=[
         'https://en.wikipedia.org/wiki/List_of_American_television_series', 
-        'https://zh.wikipedia.org/wiki/%E7%BE%8E%E5%9B%BD%E7%94%B5%E8%A7%86%E5%89%A7%E5%88%97%E8%A1%A8'
+        'https://zh.wikipedia.org/zh-cn/%E7%BE%8E%E5%9B%BD%E7%94%B5%E8%A7%86%E5%89%A7%E5%88%97%E8%A1%A8'
     ]
 
     ex_rules = [ExRule(start_urls[0], list_css_rules=list_css_rules[0]),
