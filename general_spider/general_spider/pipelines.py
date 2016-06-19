45e2926f394c04ea23583c1b4ac3d8d7ca1f86a1
geekan <geekan@foxmail.com>
2016-06-19 03:13:45 +0000

    add mysql pipeline

--- general_spider/general_spider/pipelines.py
+++ general_spider/general_spider/pipelines.py
@@ -69,3 +69,31 @@ class RedisPipeline(object):
 
     def close_spider(self, spider):
         return
+
+import sys
+import MySQLdb
+import hashlib
+from scrapy.exceptions import DropItem
+from scrapy.http import Request
+
+class MySQLStorePipeline(object):
+  def __init__(self):
+    # self.conn = MySQLdb.connect(user='user', 'passwd', 'dbname', 'host', charset="utf8", use_unicode=True)
+    self.conn = MySQLdb.connect(user='dbuser', passwd='dbpasswd', db='dbname', host='dbhost', charset="utf8", use_unicode=True)
+    self.cursor = self.conn.cursor()
+
+    def process_item(self, item, spider):    
+        try:
+            self.cursor.execute("""INSERT INTO example_book_store (book_name, price)  
+                            VALUES (%s, %s)""", 
+                           (item['book_name'].encode('utf-8'), 
+                            item['price'].encode('utf-8')))
+
+            self.conn.commit()
+
+
+        except MySQLdb.Error, e:
+            print "Error %d: %s" % (e.args[0], e.args[1])
+
+
+        return item
