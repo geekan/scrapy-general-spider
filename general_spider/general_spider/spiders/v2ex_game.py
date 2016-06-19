c075b16a7705bb1a0066680c4ca999a4170254e5
geekan <geekan@foxmail.com>
2016-06-19 21:24:39 +0800

    v2ex_game: added.

--- general_spider/general_spider/spiders/v2ex_game.py
+++ general_spider/general_spider/spiders/v2ex_game.py
@@ -28,6 +28,7 @@ class Config:
     # ex_rule = ExRule('http://www.v2ex.com/t/204079.*?$', list_css_rules=list_css_rules)
     # ex_rules = [ex_rule]
     # print(ex_rules)
+    ex_rules = []
 
     name='v2ex'
     allowed_domains = ['www.v2ex.com']
